5e36e4abb3ba867f53fa576e8e230bf570023d83
Gaston Mauro Zacarias <gastonmzacarias@gmail.com>
2021-10-13 20:21:29 -0300

    probando tests 13-10

--- test.ts
+++ test.ts
@@ -1,4 +1,22 @@
 import test from "ava";
+//imports
+import {Product} from "./index";
+import {User} from "./index";
 
-test("Testeo la clase User", (t) => {});
-test("Testeo la clase Product y sus métodos", (t) => {});
+
+test("Testeo la clase User", function (t){
+const nuevoUsuario = new User ("gaston");
+const nuevoProducto = new Product("Libro" , 5);
+nuevoUsuario.addProduct(nuevoProducto);
+
+t.is(nuevoUsuario.name , "gaston");
+t.is(nuevoProducto.name , "Libro");
+t.is(nuevoProducto.price , 5);
+});
+
+
+test("Testeo la clase Product y sus métodos",  function (t) {
+    const nuevoProducto = new Product ("Libro", 5);
+    t.is(nuevoProducto.name , "Libro");
+    t.is(nuevoProducto.price , 5);
+});
